850f6fef2414757928f287f64ee113b549da1927
Uender Carlos <44954880+uendercarlos@users.noreply.github.com>
2021-11-06 00:55:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,23 @@ def main():
         st.success('A aeração  {}'.format(result))
         #print(emprestimo)
      
-if __name__=='__main__': 
+if __name__ == "__main__":
+    import os
+
+    DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
+
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
+        "%(message)s",
+        force=True,
+    )
+
+    logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
+
+    st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+    st_webrtc_logger.setLevel(logging.DEBUG)
+
+    fsevents_logger = logging.getLogger("fsevents")
+    fsevents_logger.setLevel(logging.WARNING)
+
     main()
